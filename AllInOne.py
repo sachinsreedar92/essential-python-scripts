cf5483d40974197a803fa9c5c5b310f34bda77df
Sachin Sridhar <sachinsreedar92@users.noreply.github.com>
2019-08-24 17:53:30 +0530

    Extract,Removes Empty cell & Duplicate,Write file

---
+++ AllInOne.py
@@ -0,0 +1,23 @@
+import csv
+file = open("/Users/sachinsreedar92/Downloads/Python.txt", "w")
+with open ('/Users/sachinsreedar92/Downloads/medikoe.csv') as csvFile:
+    column = []
+    readCsv = csv.reader(csvFile, delimiter=',')
+    for row in readCsv:
+        data = row[0]
+        if data != "":
+            column.append(data)
+    dup_list = column
+    file.write(str(dup_list))
+file.close()
+
+
+def remove_duplicates(dup_list):
+    withoutDup = []
+    for ele in dup_list:
+        if ele not in withoutDup:
+            withoutDup.append(ele)
+    return withoutDup
+
+print(len(dup_list))
+print(dup_list)
